b6b55aba8f78f10f6d90d3bda42fdb9ea1f4eebb
ClaudioVass <54720366+claudiovass31876292@users.noreply.github.com>
2023-03-28 14:45:13 -0300

    contacto textbox

--- re_tp4/src/Ejercicios_tp4/Contactos.java
+++ re_tp4/src/Ejercicios_tp4/Contactos.java
@@ -6,10 +6,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JTextField;
 
 public class Contactos extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -19,12 +27,52 @@ public class Contactos extends JFrame {
 	 * Create the frame.
 	 */
 	public Contactos() {
+		setTitle("Contactos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane); // con esta propiedad estoy asociando el JPanel al JFrame
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(41, 53, 75, 14);
+		contentPane.add(lblNombre);
+		
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(41, 78, 75, 19);
+		contentPane.add(lblApellido);
+		
+		JLabel lblTelefono = new JLabel("Telefono");
+		lblTelefono.setBounds(41, 108, 75, 14);
+		contentPane.add(lblTelefono);
+		
+		JLabel lblFechaNac = new JLabel("Fecha Nac.");
+		lblFechaNac.setBounds(41, 133, 75, 14);
+		contentPane.add(lblFechaNac);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBounds(41, 219, 245, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setBounds(127, 50, 200, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(126, 77, 201, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(127, 105, 200, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(127, 130, 200, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
 	}
-
 }
