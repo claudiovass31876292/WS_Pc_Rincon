ab49beb642e8e30542c06072059610ce0766b90f
ClaudioVass <54720366+claudiovass31876292@users.noreply.github.com>
2023-04-15 20:18:23 -0300

    c
    cc

--- re_tp4/src/Ejercicios_tp4/Promedios.java
+++ re_tp4/src/Ejercicios_tp4/Promedios.java
@@ -6,28 +6,23 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
-import javax.xml.bind.ParseConversionEvent;
-
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 public class Promedios extends JFrame{
 
 	
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField txtNota1;
-	private JTextField txtNota2;
-	private JTextField txtNota3;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 	private JPanel panelInferior;
 	private Container panelSuperior;
 	private JComboBox<String>  cbTps;
@@ -35,19 +30,12 @@ public class Promedios extends JFrame{
 	private JLabel lblNota2;
 	private JLabel lblNota3;
 	private JLabel lblTps;
-	private JLabel lblPromedio;
-	private JLabel lblCondicion;
-	private JButton btnCalcular;
-	private JButton btnNuevo;
-	JButton btnSalir;
-	private JTextField txtPromedio;
-	private JTextField txtCondicion;
 	
 
 	
 	public Promedios() {
 		setTitle("Promedios");
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 515, 413);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -55,29 +43,9 @@ public class Promedios extends JFrame{
 		contentPane.setLayout(null);
 		
 		panelInferior = new JPanel();
-		panelInferior.setBorder(new TitledBorder(null, "Notas del alumno", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panelInferior.setBackground(Color.WHITE);
 		panelInferior.setBounds(22, 208, 268, 155);
 		contentPane.add(panelInferior);
-		panelInferior.setLayout(null);
-		
-		lblPromedio = new JLabel("Promedio");
-		lblPromedio.setBounds(10, 30, 46, 14);
-		panelInferior.add(lblPromedio);
-		
-		lblCondicion = new JLabel("Condicion");
-		lblCondicion.setBounds(10, 69, 46, 14);
-		panelInferior.add(lblCondicion);
-		
-		txtPromedio = new JTextField();
-		txtPromedio.setBounds(117, 27, 100, 20);
-		panelInferior.add(txtPromedio);
-		txtPromedio.setColumns(10);
-		
-		txtCondicion = new JTextField();
-		txtCondicion.setBounds(117, 66, 100, 20);
-		panelInferior.add(txtCondicion);
-		txtCondicion.setColumns(10);
 		
 		panelSuperior = new JPanel();
 		panelSuperior.setBounds(22, 60, 268, 137);
@@ -87,59 +55,44 @@ public class Promedios extends JFrame{
 		
 		
 		
-		txtNota1 = new JTextField();
-		txtNota1.setBounds(109, 20, 103, 20);
-		panelSuperior.add(txtNota1);
-		txtNota1.setColumns(10);
+		textField = new JTextField();
+		textField.setBounds(109, 20, 103, 20);
+		panelSuperior.add(textField);
+		textField.setColumns(10);
 		
-		txtNota2 = new JTextField();
-		txtNota2.setBounds(109, 45, 103, 20);
-		panelSuperior.add(txtNota2);
-		txtNota2.setColumns(10);
+		textField_1 = new JTextField();
+		textField_1.setBounds(109, 45, 103, 20);
+		panelSuperior.add(textField_1);
+		textField_1.setColumns(10);
 		
-		txtNota3 = new JTextField();
-		txtNota3.setBounds(109, 70, 103, 20);
-		panelSuperior.add(txtNota3);
-		txtNota3.setColumns(10);
+		textField_2 = new JTextField();
+		textField_2.setBounds(109, 70, 103, 20);
+		panelSuperior.add(textField_2);
+		textField_2.setColumns(10);
 		
 		cbTps = new JComboBox<String>();
 		cbTps.setBounds(109, 98, 103, 20);
 		panelSuperior.add(cbTps);
 		
-		btnCalcular = new JButton("CALCULAR ");
+		JButton btnCalcular = new JButton("CALCULAR ");
 		btnCalcular.addActionListener(new eventoCalcular());
 		btnCalcular.setBounds(319, 60, 123, 35);
 		contentPane.add(btnCalcular);
 		
-		btnNuevo = new JButton("NUEVO");
-		btnNuevo.addActionListener(new eventoNuevo());
+		JButton btnNuevo = new JButton("NUEVO");
 		btnNuevo.setBounds(319, 99, 123, 35);
 		contentPane.add(btnNuevo);
 		
-		btnSalir = new JButton("SALIR");
-		btnSalir.addActionListener(new eventoSalir());
-		btnSalir.setBounds(319, 141, 123, 35);
-		contentPane.add(btnSalir);
-		cbTps.addItem("APROBADO");
-		cbTps.addItem("DESAPROBADO");
+		JButton btnNewButton_2 = new JButton("SALIR");
+		btnNewButton_2.setBounds(319, 141, 123, 35);
+		contentPane.add(btnNewButton_2);
+		cbTps.addItem("tp1");
+		cbTps.addItem("tp2");
+		cbTps.addItem("tp3");
 		
 		creacionLabel();
 	}
-
-	private boolean isInt(String str) {
-		try 
-		{
-		Integer.parseInt(str);
-		return true;
-		}
-		catch (Throwable e )
-		{
-			return false;
-		}
 	
-	}
-
-
 	/*static JPanel creaBorde( javax.swing.border.Border b ) {
 	    JPanel panel = new JPanel();
 	    String str = b.getClass().toString();
@@ -157,62 +110,14 @@ public class Promedios extends JFrame{
 	
 			public void actionPerformed(ActionEvent arg0) {
 		    
-				String n1 = txtNota1.getText().trim();
-				String n2 = txtNota2.getText().trim();
-				String n3 = txtNota3.getText().trim();
+				// aca tiene desplegar los resultados en el panelInferior
+				System.out.println("aca tiene desplegar los resultados en el panelInferior");
 				
-				if(n1.isEmpty() || n2.isEmpty() || n3.isEmpty())
-				{
-				 JOptionPane.showMessageDialog(contentPane, "Complete todos los campos","Error", JOptionPane.ERROR_MESSAGE);
-				 return;
-				}
-				
-				if(!isInt(n1) || !isInt(n2) || !isInt(n3))
-				{
-					JOptionPane.showMessageDialog(contentPane, "Ingrese un valor numerico","Error",JOptionPane.ERROR_MESSAGE);
-					return;
-				}
-				
-				float nota1 = Integer.parseInt(n1);
-				float nota2 = Integer.parseInt(n2);
-				float nota3 = Integer.parseInt(n3);
-				float prom = (nota1+nota2+nota3)/3;
-				txtPromedio.setText(prom+"");
 				
 			}
-
+		
 	}
 	
-      public class eventoNuevo implements ActionListener
-      {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			txtNota1.setText("");
-			txtNota2.setText("");
-			txtNota3.setText("");
-			txtCondicion.setText("");
-			txtPromedio.setText("");
-			
-		}
-    	  
-      }
-
-      public class eventoSalir implements ActionListener
-      {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			// hacer que cierre la ventana al clikeat el boton 
-		System.exit(JFrame.DISPOSE_ON_CLOSE);
-		
-			
-		}
-    	  
-      }
-      
 	public void creacionLabel()
 	{
 		lblNota1 = new JLabel("Nota 1 :");
